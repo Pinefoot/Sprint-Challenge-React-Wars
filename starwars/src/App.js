301c50f5867d956075358b1b9fadbfb812659466
Pinefoot <jameslcarpino@gmail.com>
2020-03-20 08:28:55 -0700

    axios up and running, getting back my data. it's mapping correctly. setting up the props now.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import MainPage from "./components/MainPage";
+
+
 
 const App = () => {
+
+  const [starwarsData, setStarwarsData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people")
+      .then(response => {
+        console.log(response.data.results);
+        setStarwarsData(response.data.results);
+      })
+      .catch(error => {
+        console.log('use the force to find your error', error);
+      });
+    
+  }, []);
+
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +33,15 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {starwarsData.map((data, index) => {
+        console.log(data);
+        return (
+          <MainPage
+            key = {index}
+
+          />
+        )
+      })}
     </div>
   );
 }
