1e9e164f2cb47ff41c7b6e8ea0dcb54871a365cb
Pinefoot <jameslcarpino@gmail.com>
2020-03-20 09:53:30 -0700

    completed MVP with one elment on the page, and styled.

--- starwars/src/components/MainPage.js
+++ starwars/src/components/MainPage.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import styled from "styled-components";
 
+
+const Container = styled.div`
+   margin: 0 auto;
+   width: 40%;
+   border-right: 2px #FFE81F solid;
+   border-top: 2px #FFE81F solid;
+   border-bottom: 2px black solid;
+   border-left: 2px black solid;
+   margin-top: 2%;
+`;
+
+const Titler = styled.h2`
+    font-family: 'Fira Sans', sans-serif;
+    color: Yellow;
+    text-shadow: 2px 2px black;
+`;
 
 const MainPage = props => {
     return (
-        <div>
-            <h2>{props.name}</h2>
+        <Container>
+            <Titler>{props.name}</Titler>
             {/* <img src={props.url} alt='starwars characters'></img>
             
             <p>{props.birthYear}</p>
             <img src={props.homeworld} alt='homeworlds'/> */}
             
-
-
-
-
-
-        </div>
+        </Container>
 
     )
 
